b62c66d8b3b64e186f3beab25c047f2b329126c7
Sudheer Parasker <sparasker@gmail.com>
2013-09-12 19:37:51 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ulp::Application.config.secret_key_base = '7f3c709ed3ff2582321cf8ea05f4a5e8b67405114423442a103256d8bcada43bc5448c5e8f1fdcff06adea82efce12f2975d2f12b718d542f5ddb550cff2b856'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ulp::Application.config.session_store :cookie_store, key: '_ulp_session'
